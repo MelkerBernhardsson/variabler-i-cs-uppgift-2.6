e0eec8abbb28b1c03c8ad9d19192a79031096bbb
Pingvu <nhlpool27@gmail.com>
2023-01-25 10:44:36 +0100

    Add project files.

---
+++ variabler i cs uppgift 2.6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace uppgift6
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("skriv ett tal"); // om du skriver decimal använd , och inte . !!!//
+            double x = double.Parse(Console.ReadLine());
+            Console.WriteLine("skriv ett till tal");
+            double y = double.Parse(Console.ReadLine());
+            double medel = (x + y) / 2;
+            Console.WriteLine("medelvärdet mellan dina två tal är " + medel);
+        }
+    }
+}
